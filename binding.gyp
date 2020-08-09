a5c396839015352cea810263f89dfd609ddf96fe
Alex Plate <AlexPl292@gmail.com>
2020-08-09 10:09:38 +1000

    Remove all mentios of IJVM

--- binding.gyp
+++ binding.gyp
@@ -1,7 +1,7 @@
 {
   "targets": [
     {
-      "target_name": "tree_sitter_IJVM_binding",
+      "target_name": "tree_sitter_jas_binding",
       "include_dirs": [
         "<!(node -e \"require('nan')\")",
         "src"
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 try {
-  module.exports = require("./build/Release/tree_sitter_IJVM_binding");
+  module.exports = require("./build/Release/tree_sitter_jas_binding");
 } catch (error) {
   try {
-    module.exports = require("./build/Debug/tree_sitter_IJVM_binding");
+    module.exports = require("./build/Debug/tree_sitter_jas_binding");
   } catch (_) {
     throw error
   }
